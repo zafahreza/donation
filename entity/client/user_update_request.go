13a7b8f2e172b33b272861884dfcb396090dc82d
zafahereza <fahrezam6@gmail.com>
2022-12-01 22:38:27 +0700

    littel change bug

--- entity/client/user_update_request.go
+++ entity/client/user_update_request.go
@@ -2,7 +2,7 @@ package client
 
 type UserUpdateRequest struct {
 	Id        int    `validate:"required" json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `validate:"email" json:"email"`
+	FirstName string `validate:"required" json:"first_name"`
+	LastName  string `validate:"required" json:"last_name"`
+	Email     string `validate:"required,email" json:"email"`
 }
